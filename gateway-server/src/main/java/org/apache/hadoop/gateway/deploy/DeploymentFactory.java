3dbafb23cc9eac9ce1522e297a915893cdce04b3
Sumit Gupta <sumit@apache.org>
2015-01-27 10:56:37 -0500

    Added versions to service definitions for deployment and topology KNOX-481

--- gateway-server/src/main/java/org/apache/hadoop/gateway/deploy/DeploymentFactory.java
+++ gateway-server/src/main/java/org/apache/hadoop/gateway/deploy/DeploymentFactory.java
@@ -43,15 +43,7 @@ import java.beans.Statement;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.ServiceLoader;
-import java.util.Set;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 public abstract class DeploymentFactory {
 
